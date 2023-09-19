f5cd379aa640b5a74e5d04505388e2db08559534
Clinton <+>
2023-09-19 22:43:41 +0300

    completed assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,56 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
+    if admin_login == ("sudo", "12345"):
+        "Access denied"    
+    elif admin_login == ("admin", "12345"):
+        "Access granted"
+    elif admin_login == ("ADMIN", "12345"):
+        "Access granted"
     # your code here
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature >= 40:
+       "it's a brisk out there."
+    elif temperature == 40  < 65:
+        "It's a little chilly out there!"
+    elif temperature > 85:
+         "It's too dang hot out there!"
+    else:
+        "It's perfect out there!"
+       # your code here
     pass
 
 def fizzbuzz(num):
+    if fizzbuzz(1):
+        "1"
+    elif fizzbuzz(2):
+        "2"
+    elif fizzbuzz(3):
+        "Fizz"
+    elif fizzbuzz(4):
+        "4"
+    elif fizzbuzz(5):
+        "Buzz"
+    elif fizzbuzz(6):
+        "fizzbuzz"
     # your code here
     pass
 
 def calculator(operation, num1, num2):
+    num = "(";1,2,3,4,6,7,8,9;")"
+    if (num1*num2):
+        "num1*num2"
+    elif (num1+num2):
+        "num1+num2"
+    elif (num1/num2):
+        "num1/num2" 
+    else:"Invalid operation"
+                    
+      
+
+    
+
     # your code here
     pass
